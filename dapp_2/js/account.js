14ebc348a5b869cde7300b8158b336fd7b26002f
IeRae <duddml314@gmail.com>
2017-12-31 00:27:23 +0900

    Add files via upload

--- dapp_2/js/account.js
+++ dapp_2/js/account.js
@@ -5,7 +5,7 @@ var savedBalances = 0;
 setInterval(function() {
 
   var contractAddress = '0xb20083039a3b7b76c0dc3884c6e5f41c3784671d';
-  var abi = [{"constant":false,"inputs":[{"name":"_receiver","type":"address"},{"name":"_message","type":"string"}],"name":"sendMsg","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getMsg","outputs":[{"name":"","type":"uint256"},{"name":"","type":"address"},{"name":"","type":"string"},{"name":"","type":"bool"}],"payable":false,"type":"function"}];
+  var abi = [{"constant":false,"inputs":[{"name":"newAddress","type":"string"},{"name":"newMessage","type":"string"},{"name":"newMaster","type":"uint256"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
   var message;
 
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
@@ -14,7 +14,7 @@ setInterval(function() {
     window.web3 = new Web3(web3.currentProvider);
     message = web3.eth.contract(abi).at(contractAddress);
     
-    message.getMsg(function(e,r) {
+    message.get(function(e,r) {
 
         if(Number(blockNumber) != r[0]) {
 
--- dapp_2/js/modal.js
+++ dapp_2/js/modal.js
@@ -33,18 +33,19 @@ window.onload = function () {
             if(address.search(/0x[a-zA-Z0-9]{40}/) != -1) {
         
               var contractAddress = '0xb20083039a3b7b76c0dc3884c6e5f41c3784671d';
-              var abi = [{"constant":false,"inputs":[{"name":"_receiver","type":"address"},{"name":"_message","type":"string"}],"name":"sendMsg","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getMsg","outputs":[{"name":"","type":"uint256"},{"name":"","type":"address"},{"name":"","type":"string"},{"name":"","type":"bool"}],"payable":false,"type":"function"}];
+              var abi = [{"constant":false,"inputs":[{"name":"newAddress","type":"string"},{"name":"newMessage","type":"string"},{"name":"newMaster","type":"uint256"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
               var sendMessage;
 
               window.web3 = new Web3(web3.currentProvider);
 
               sendMessage = web3.eth.contract(abi).at(contractAddress);
-              sendMessage.sendMsg(address, document.getElementById('receive_contents').value, {gasPrice:web3.toWei(2, 'Gwei')}, function(e,r) {
+			  sendMessage.set(JSON.parse(address),document.getElementById('receive_contents').value,1,{gasPrice:web3.toWei(2, 'Gwei')}, function(e,r) {
 
                 alert("TXID Copy to clipboard: Ctrl+C, Enter\n" + r);
 
               });
-
+			
+			//초기화 부분
               document.getElementById('receive_address').value = "";
               document.getElementById('receive_contents').value = "";
               this.parentNode.close();
@@ -61,16 +62,26 @@ window.onload = function () {
     receiveModalBtn.addEventListener('click', function(e) {
 
       var contractAddress = '0xb20083039a3b7b76c0dc3884c6e5f41c3784671d';
-      var abi = [{"constant":false,"inputs":[{"name":"_receiver","type":"address"},{"name":"_message","type":"string"}],"name":"sendMsg","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getMsg","outputs":[{"name":"","type":"uint256"},{"name":"","type":"address"},{"name":"","type":"string"},{"name":"","type":"bool"}],"payable":false,"type":"function"}];
+      var abi = [{"constant":false,"inputs":[{"name":"newAddress","type":"string"},{"name":"newMessage","type":"string"},{"name":"newMaster","type":"uint256"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
       var message;
-
+	  var addressResult;
+	  var addressContent;
+	  var messageContent;
+	  var masterContent;
+	  
       window.web3 = new Web3(web3.currentProvider);
       message = web3.eth.contract(abi).at(contractAddress);
 
-      message.getMsg(function(e,r) {
-
-        document.getElementById('send_address').value = r[1];
-        document.getElementById('send_contents').value = r[2];
+      message.get(function(e,r) {
+		
+		addressResult = r[1];
+		messageContent = r[2];
+		masterContent = r[3];
+		
+		addressContent = JSON.stringify(addressResult);
+		
+        document.getElementById('send_address').value = addressResult;
+        document.getElementById('send_contents').value = messageContent;
 
       });
 
